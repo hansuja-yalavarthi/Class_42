42a04fcfde629c7937d01061b0af8b3e3cd2203e
hansuja-yalavarthi <78600451+hansuja-yalavarthi@users.noreply.github.com>
2021-05-25 12:52:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var name = "Hansuja";
+var number = parseInt("123456");
+var v1 = parseInt("3.14");
+var test = null;
+
+//Print Hansuja
+console.log(name);
+
+console.log(name.length);
+
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+
+console.log(parseInt(number));
+
+console.log(typeof (number));
+console.log(typeof (name));
+
+document.write('Using parseInt("3.14") = ' + v1 + "<br>");
+document.write(typeof number);
+
+console.log("Hi" + " " + "Friend");
+
+console.log(test);
